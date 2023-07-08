a560fdd849ce84097a3b5594390a69320ce8a6f1
Sam Rose <hello@samwho.dev>
2023-07-08 16:45:22 +0100

    Add tests, update README.

---
+++ README.md
@@ -0,0 +1,76 @@
+# recolor
+
+`recolor` is a tool to help you colourise the output of any command. It works
+using familiar regular expressions, and gives you lots of flexibility and
+control in what the output looks like.
+
+![](images/demo.gif)
+
+## How it works
+
+`recolor` reads input line by line and applies the regular expression you
+specify to each line. It colours capture groups only, and if you name your
+capture groups you can customise the colours used.
+
+As an example, the following will colour only the 2nd octet of any IPv4
+addresses output by `ping`:
+
+```bash
+$ ping google.com | recolor "\d{1,3}\.(\d{1,3})\.\d{1,3}\.\d{1,3}"
+```
+
+![](images/example-ping-1.png)
+
+We could colour all 4 octets separately, `recolor` has a set of default
+colours it cycles through each new capture group:
+
+![](images/example-ping-2.png)
+
+If we wanted to control the styling, we can name our capture groups and
+specify the styling after the regex.
+
+```bash
+$ ping google.com | recolor "(?P<one>\d{1,3})\.(?P<two>\d{1,3})\.(?P<three>\d{1,3})\.(?P<four>\d{1,3})" one=red,bold two=green,underline three=yellow,dim four=black
+```
+
+![](images/example-ping-3.png)
+
+# FAQ
+
+## Does it support multiline regular expressions?
+
+No, and I don't have plans to. It would make the code significantly more
+complicated. If it's something you really, really want I would happily review a
+PR, though.
+
+## What styles are available?
+
+Here's a list of all of the words you can use as styles:
+
+```
+black
+red
+green
+yellow
+blue
+magenta
+cyan
+white
+bright_black
+bright_red
+bright_green
+bright_yellow
+bright_blue
+bright_magenta
+bright_cyan
+bright_white
+bold | bolded
+dimmed | dim
+italic | italics
+underline | underlined
+blink | blinking
+hidden
+strikethrough | struckthrough | strike
+```
+
+You can also specify colours as CSS style hex values, e.g. `#ff0000` for red.
--- src/main.rs
+++ src/main.rs
@@ -17,51 +17,73 @@ struct Args {
     regex: String,
 
     /// Color map.
-    #[arg(value_parser = parse_colors)]
-    colors: Option<HashMap<String, Style>>,
+    #[arg()]
+    colors: Vec<String>,
 }
 
 lazy_static! {
-    static ref COLORS: HashMap<String, Style> = {
-        let mut map = HashMap::new();
-        map.insert("black".to_string(), Style::new().black());
-        map.insert("red".to_string(), Style::new().red());
-        map.insert("green".to_string(), Style::new().green());
-        map.insert("yellow".to_string(), Style::new().yellow());
-        map.insert("blue".to_string(), Style::new().blue());
-        map.insert("magenta".to_string(), Style::new().magenta());
-        map.insert("cyan".to_string(), Style::new().cyan());
-        map.insert("white".to_string(), Style::new().white());
-        map.insert("bright_black".to_string(), Style::new().bright_black());
-        map.insert("bright_red".to_string(), Style::new().bright_red());
-        map.insert("bright_green".to_string(), Style::new().bright_green());
-        map.insert("bright_yellow".to_string(), Style::new().bright_yellow());
-        map.insert("bright_blue".to_string(), Style::new().bright_blue());
-        map.insert("bright_magenta".to_string(), Style::new().bright_magenta());
-        map.insert("bright_cyan".to_string(), Style::new().bright_cyan());
-        map.insert("bright_white".to_string(), Style::new().bright_white());
-        map
-    };
-    static ref COLOR_VEC: Vec<Style> = {
+    static ref DEFAULT_COLORS: Vec<Style> = {
         let mut vec = Vec::new();
-        for (_, color) in COLORS.iter() {
-            vec.push(*color);
-        }
+        vec.push(Style::new().red());
+        vec.push(Style::new().green());
+        vec.push(Style::new().yellow());
+        vec.push(Style::new().blue());
+        vec.push(Style::new().magenta());
+        vec.push(Style::new().cyan());
+        vec.push(Style::new().white());
         vec
     };
 }
 
 fn parse_color(s: &str) -> Result<Style> {
-    COLORS
-        .get(s)
-        .copied()
-        .ok_or_else(|| anyhow::anyhow!(format!("invalid color \"{}\"", s)))
+    let mut style = Style::new();
+    for part in s.split(",") {
+        if part.starts_with("#") {
+            if part.len() != 7 {
+                return Err(anyhow::anyhow!(format!("invalid hex color: \"{}\"", s)));
+            }
+            let (r, g, b) = (
+                u8::from_str_radix(&part[1..3], 16)?,
+                u8::from_str_radix(&part[3..5], 16)?,
+                u8::from_str_radix(&part[5..7], 16)?,
+            );
+            style = style.truecolor(r, g, b);
+            continue;
+        }
+        style = match part {
+            "black" => style.black(),
+            "red" => style.red(),
+            "green" => style.green(),
+            "yellow" => style.yellow(),
+            "blue" => style.blue(),
+            "magenta" => style.magenta(),
+            "cyan" => style.cyan(),
+            "white" => style.white(),
+            "bright_black" => style.bright_black(),
+            "bright_red" => style.bright_red(),
+            "bright_green" => style.bright_green(),
+            "bright_yellow" => style.bright_yellow(),
+            "bright_blue" => style.bright_blue(),
+            "bright_magenta" => style.bright_magenta(),
+            "bright_cyan" => style.bright_cyan(),
+            "bright_white" => style.bright_white(),
+            "bold" | "bolded" => style.bold(),
+            "dimmed" | "dim" => style.dimmed(),
+            "italic" | "italics" => style.italic(),
+            "underline" | "underlined" => style.underline(),
+            "blink" | "blinking" => style.blink(),
+            "hidden" => style.hidden(),
+            "strikethrough" | "struckthrough" | "strike" => style.strikethrough(),
+            _ => return Err(anyhow::anyhow!(format!("invalid style: \"{}\"", s))),
+        };
+    }
+    Ok(style)
 }
 
-fn parse_colors(s: &str) -> Result<HashMap<String, Style>> {
+fn parse_styles(colors: Vec<String>) -> Result<HashMap<String, Style>> {
     let mut map = HashMap::new();
-    for pair in s.split(',') {
-        let mut pair = pair.split('=');
+    for color in colors {
+        let mut pair = color.split('=');
         let key = pair
             .next()
             .context("invalid colors, format is key=value,key=value")?;
@@ -74,37 +96,65 @@ fn parse_colors(s: &str) -> Result<HashMap<String, Style>> {
     Ok(map)
 }
 
+enum Op {
+    Push(Style),
+    Pop,
+}
+
 fn run(input: impl BufRead, mut output: impl Write, args: Args) -> Result<()> {
     let regex = Regex::new(&args.regex).context("invalid regex")?;
-    let colors = args.colors.unwrap_or_default();
+    let styles = parse_styles(args.colors)?;
+
+    let mut ops_by_position: HashMap<usize, Vec<Op>> = HashMap::new();
+    let mut style_stack: Vec<Style> = Vec::new();
 
     for line in input.lines() {
+        ops_by_position.clear();
+        style_stack.clear();
+
         let line = line?;
-        let mut last_match_end = 0;
         for m in regex.captures_iter(&line) {
             for (i, capture) in m.iter().enumerate().skip(1) {
-                let color = match regex.capture_names().nth(i) {
-                    Some(Some(name)) => colors
+                let style = match regex.capture_names().nth(i) {
+                    Some(Some(name)) => styles
                         .get(name)
                         .copied()
-                        .unwrap_or(COLOR_VEC[i % COLOR_VEC.len()]),
-                    _ => COLOR_VEC[i % COLOR_VEC.len()],
+                        .unwrap_or(DEFAULT_COLORS[i % DEFAULT_COLORS.len()]),
+                    _ => DEFAULT_COLORS[i % DEFAULT_COLORS.len()],
                 };
+
                 if let Some(mat) = capture {
-                    write!(
-                        output,
-                        "{}{}",
-                        &line[last_match_end..mat.start()],
-                        &line[mat.start()..mat.end()].to_owned().style(color)
-                    )?;
-                    last_match_end = mat.end();
+                    ops_by_position
+                        .entry(mat.start())
+                        .or_default()
+                        .push(Op::Push(style));
+
+                    ops_by_position.entry(mat.end()).or_default().push(Op::Pop);
                 }
             }
         }
-        if last_match_end < line.len() {
-            write!(output, "{}", &line[last_match_end..])?;
+
+        let mut buf = String::new();
+        for (position, char) in line.char_indices() {
+            if let Some(ops) = ops_by_position.get(&position) {
+                let style = style_stack.last().copied().unwrap_or_default();
+                write!(output, "{}", buf.style(style))?;
+                buf.clear();
+
+                for op in ops {
+                    match op {
+                        Op::Push(style) => style_stack.push(*style),
+                        Op::Pop => {
+                            style_stack.pop();
+                        }
+                    }
+                }
+            }
+            buf.push(char);
         }
-        write!(output, "\n")?;
+        let style = style_stack.last().copied().unwrap_or_default();
+        write!(output, "{}", buf.style(style))?;
+        writeln!(output)?;
     }
 
     Ok(())
@@ -118,4 +168,90 @@ fn main() -> Result<()> {
     debug!("args: {:?}", args);
 
     run(stdin().lock(), stdout().lock(), args)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Cursor;
+    use test_case::test_case;
+
+    #[test_case(
+        vec!["(foo)"],
+        "hello foo",
+        format!("hello {}\n", "foo".style(DEFAULT_COLORS[1]))
+        ; "single match")
+    ]
+    #[test_case(
+        vec!["(foo)(bar)"],
+        "hello foobar",
+        format!("hello {}{}\n", "foo".style(DEFAULT_COLORS[1]), "bar".style(DEFAULT_COLORS[2]))
+        ; "multiple match")
+    ]
+    #[test_case(
+        vec!["(?P<foo>foo)(?P<bar>bar)", "foo=green", "bar=red"],
+        "hello foobar",
+        format!(
+            "hello {}{}\n",
+            "foo".style(Style::new().green()),
+            "bar".style(Style::new().red())
+        )
+        ; "named matches")
+    ]
+    #[test_case(
+        vec!["(5)"],
+        "12345 12345 12345",
+        format!(
+            "1234{0} 1234{0} 1234{0}\n",
+            "5".style(DEFAULT_COLORS[1]),
+        )
+        ; "multiple single match")
+    ]
+    #[test_case(
+        vec!["(5)"],
+        "hello world",
+        "hello world\n"
+        ; "no matches")
+    ]
+    #[test_case(
+        vec!["(?P<five>5)", "five=#ff0000,underline"],
+        "12345 12345 12345",
+        format!(
+            "1234{0} 1234{0} 1234{0}\n",
+            "5".style(Style::new().truecolor(255, 0, 0).underline()),
+        )
+        ; "CSS colors")
+    ]
+    #[test_case(
+        vec!["123(5)"],
+        "12345 12345 1235",
+        format!(
+            "12345 12345 123{0}\n",
+            "5".style(DEFAULT_COLORS[1]),
+        )
+        ; "regex with non-capture group component")
+    ]
+    #[test_case(
+        vec!["12(3(5))"],
+        "12345 12345 1235",
+        format!(
+            "12345 12345 12{}{}\n",
+            "3".style(DEFAULT_COLORS[1]),
+            "5".style(DEFAULT_COLORS[2]),
+        )
+        ; "capture group inside another capture group")
+    ]
+    fn test_success(
+        args: impl Into<Vec<&'static str>>,
+        input: impl Into<String>,
+        expected_output: impl Into<String>,
+    ) -> Result<()> {
+        let mut output = Vec::new();
+        let mut args: Vec<&str> = args.into();
+        args.insert(0, "recolor");
+        let args = Args::parse_from(args);
+        run(Cursor::new(input.into()), &mut output, args)?;
+        assert_eq!(String::from_utf8(output)?, expected_output.into());
+        Ok(())
+    }
 }
